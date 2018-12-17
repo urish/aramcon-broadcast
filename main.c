fed3556e6d58ed7f7e61b55028940abdabb2ba92
Uri Shaked <uri@urishaked.com>
2018-12-17 20:39:06 +0200

    feat: Implement serial uart -> vs1053 player
    Any mp3 data you send over the serial port is sent directly to the
    VS1003B/VS1053B and played there

--- main.c
+++ main.c
@@ -42,9 +42,7 @@
 #include <string.h>
 #include "sdk_common.h"
 #include "nrf.h"
-#include "nrf_esb.h"
 #include "nrf_error.h"
-#include "nrf_esb_error_codes.h"
 #include "nrf_delay.h"
 #include "nrf_delay.h"
 #include "nrf_drv_clock.h"
@@ -65,27 +63,119 @@
 
 #define MAX_BUF_SIZE 32
 
-// -- ESB DEFINES --
-static nrf_esb_payload_t        rx_payload;
-// -- END OF ESB DEFINES
+// -- USB DEFINES --
+static void cdc_acm_user_ev_handler(app_usbd_class_inst_t const * p_inst,
+                                    app_usbd_cdc_acm_user_event_t event);
 
-// -- ESB CODE --
+#define CDC_ACM_COMM_INTERFACE  0
+#define CDC_ACM_COMM_EPIN       NRF_DRV_USBD_EPIN2
 
-void nrf_esb_event_handler(nrf_esb_evt_t const * p_event)
+#define CDC_ACM_DATA_INTERFACE  1
+#define CDC_ACM_DATA_EPIN       NRF_DRV_USBD_EPIN1
+#define CDC_ACM_DATA_EPOUT      NRF_DRV_USBD_EPOUT1
+
+/** @brief CDC_ACM class instance */
+APP_USBD_CDC_ACM_GLOBAL_DEF(m_app_cdc_acm,
+                            cdc_acm_user_ev_handler,
+                            CDC_ACM_COMM_INTERFACE,
+                            CDC_ACM_DATA_INTERFACE,
+                            CDC_ACM_COMM_EPIN,
+                            CDC_ACM_DATA_EPIN,
+                            CDC_ACM_DATA_EPOUT,
+                            APP_USBD_CDC_COMM_PROTOCOL_AT_V250);
+
+char mp3_tx_buf[MAX_BUF_SIZE];
+
+// -- USB Code --
+
+// USB CODE START
+static bool m_usb_connected = false;
+
+
+/** @brief User event handler @ref app_usbd_cdc_acm_user_ev_handler_t */
+static void cdc_acm_user_ev_handler(app_usbd_class_inst_t const * p_inst,
+                                    app_usbd_cdc_acm_user_event_t event)
 {
-    switch (p_event->evt_id)
+    app_usbd_cdc_acm_t const * p_cdc_acm = app_usbd_cdc_acm_class_get(p_inst);
+
+    switch (event)
     {
-        case NRF_ESB_EVENT_TX_SUCCESS:
-            NRF_LOG_DEBUG("TX SUCCESS EVENT");
+        case APP_USBD_CDC_ACM_USER_EVT_PORT_OPEN:
+        {
+            NRF_LOG_INFO("CDC ACM port opened");
+
+            /* Set up the first transfer */
+            ret_code_t ret = app_usbd_cdc_acm_read(p_cdc_acm, mp3_tx_buf, MAX_BUF_SIZE);
+            VERIFY_SUCCESS(ret);
             break;
-        case NRF_ESB_EVENT_TX_FAILED:
-            NRF_LOG_DEBUG("TX FAILED EVENT");
-            (void) nrf_esb_flush_tx();
-            (void) nrf_esb_start_tx();
+        }
+
+        case APP_USBD_CDC_ACM_USER_EVT_PORT_CLOSE:
+            NRF_LOG_INFO("CDC ACM port closed");
             break;
 
-        case NRF_ESB_EVENT_RX_RECEIVED:
-            NRF_LOG_DEBUG("RX RECEIVED EVENT");
+        case APP_USBD_CDC_ACM_USER_EVT_TX_DONE:
+            break;
+
+        case APP_USBD_CDC_ACM_USER_EVT_RX_DONE:
+        {
+            ret_code_t ret;
+
+            vs1053_send_data(mp3_tx_buf, sizeof(mp3_tx_buf));
+
+            ret = app_usbd_cdc_acm_read(p_cdc_acm, mp3_tx_buf, MAX_BUF_SIZE);
+            VERIFY_SUCCESS(ret);
+
+            break;
+        }
+        default:
+            break;
+    }
+}
+
+static void usbd_user_ev_handler(app_usbd_event_type_t event)
+{
+    switch (event)
+    {
+        case APP_USBD_EVT_DRV_SUSPEND:
+            break;
+
+        case APP_USBD_EVT_DRV_RESUME:
+            break;
+
+        case APP_USBD_EVT_STARTED:
+            break;
+
+        case APP_USBD_EVT_STOPPED:
+            app_usbd_disable();
+            break;
+
+        case APP_USBD_EVT_POWER_DETECTED:
+            NRF_LOG_INFO("USB power detected");
+
+            if (!nrf_drv_usbd_is_enabled())
+            {
+                app_usbd_enable();
+            }
+            break;
+
+        case APP_USBD_EVT_POWER_REMOVED:
+        {
+            NRF_LOG_INFO("USB power removed");
+            m_usb_connected = false;
+            app_usbd_stop();
+        }
+            break;
+
+        case APP_USBD_EVT_POWER_READY:
+        {
+            NRF_LOG_INFO("USB ready");
+            m_usb_connected = true;
+            app_usbd_start();
+        }
+            break;
+
+        default:
             break;
     }
 }
@@ -98,36 +188,12 @@ void clocks_start( void )
     while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0);
 }
 
-uint32_t esb_init( void )
-{
-    uint32_t err_code;
-    uint8_t base_addr_0[4] = {0xE7, 0xE7, 0xE7, 0xE7};
-    uint8_t base_addr_1[4] = {0xC2, 0xC2, 0xC2, 0xC2};
-    uint8_t addr_prefix[8] = {0xE7, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8 };
-
-    nrf_esb_config_t nrf_esb_config         = NRF_ESB_DEFAULT_CONFIG;
-    nrf_esb_config.event_handler            = nrf_esb_event_handler;
-    nrf_esb_config.mode                     = NRF_ESB_MODE_PRX;
-
-    err_code = nrf_esb_init(&nrf_esb_config);
-
-    VERIFY_SUCCESS(err_code);
-
-    err_code = nrf_esb_set_base_address_0(base_addr_0);
-    VERIFY_SUCCESS(err_code);
-
-    err_code = nrf_esb_set_base_address_1(base_addr_1);
-    VERIFY_SUCCESS(err_code);
-
-    err_code = nrf_esb_set_prefixes(addr_prefix, NRF_ESB_PIPE_COUNT);
-    VERIFY_SUCCESS(err_code);
-
-    return err_code;
-}
-
 int main(void)
 {
     ret_code_t err_code;
+    static const app_usbd_config_t usbd_config = {
+        .ev_state_proc = usbd_user_ev_handler
+    };
 
     printf("Init\n");
 
@@ -144,26 +210,34 @@ int main(void)
 
     app_timer_init();
 
-    err_code = esb_init();
-    APP_ERROR_CHECK(err_code);
-
     vs1053_init();
 
     vs1053_set_volume(0, 0); // Maximum volume
 
-    err_code = nrf_esb_start_rx();
-    APP_ERROR_CHECK(err_code);
+    NRF_LOG_DEBUG("Starting USB...");
+
+    app_usbd_serial_num_generate();
+    ret = app_usbd_init(&usbd_config);
+    APP_ERROR_CHECK(ret);
+
+    app_usbd_class_inst_t const * class_cdc_acm = app_usbd_cdc_acm_class_inst_get(&m_app_cdc_acm);
+    ret = app_usbd_class_append(class_cdc_acm);
+    APP_ERROR_CHECK(ret);
+
+    ret = app_usbd_power_events_enable();
+    APP_ERROR_CHECK(ret);
 
     printf("Start Recv-Play loop...\n");
 
-    // Process ESB packets forever, send them to the MP3 player
-    for (;;) {
-        while (nrf_esb_read_rx_payload(&rx_payload) == NRF_SUCCESS) {
-            if (rx_payload.length > 0) {
-                vs1053_send_data(rx_payload.data, rx_payload.length);
-            }
+    // Process USB events forever
+    for (;;)
+    {
+        while (app_usbd_event_queue_process())
+        {
+            /* Nothing to do */
         }
 
+        /* Sleep CPU only if there was no interrupt since last loop processing */
         __WFE();
     }
 }
